091dad8b534493d52f7e9611d38965e661374418
caicaijoe <664997004@qq.com>
2026-03-15 01:53:34 +0800

    Add PWA runtime asset: pages/_app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,21 @@
+import Head from "next/head";
+import PwaRuntime from "../components/PwaRuntime";
 import "../app/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icons/icon-192.png" />
+        <meta name="application-name" content="JOEAGENT" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <meta name="apple-mobile-web-app-title" content="JOEAGENT" />
+      </Head>
+      <PwaRuntime />
+      <Component {...pageProps} />
+    </>
+  );
 }
